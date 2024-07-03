567b614e88138d9dc6c3b6f6169af5963a45c131
Divyanshu Singh <divyanshusingh2903@gmail.com>
2024-07-03 16:18:39 -0500

    frontend project push

---
+++ frontend/Eureka-app/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Eureka_app
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
